d1b60b09285d28a743024d119226baa26504f065
amonradev <amonracardoso@gmail.com>
2021-11-17 21:22:56 -0300

    Adicionando comentarios

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,11 +6,13 @@ export const Home = () => {
     const nomeUsuario = "Fernando"
     return (
         <ScrollView style={style.MasterBox}>
+            {/* Acho que isso tambem pode ser um componente */}
             <ImageBackground source={require('../../img/background.png')} 
                             imageStyle={{opacity:0.15}} 
                             style={style.Imagem}>
                         <View style={style.PerfilBox}>
                             <Image source={require('../../img/perfil.png')} style={style.ImagemPerfil} resizeMode="contain" />
+                            {/* Criar um componente pra isso aqui em baixo */}
                             <Text style={style.TextoUsuario}>
                                 Bem Vindo, 
                             </Text>
