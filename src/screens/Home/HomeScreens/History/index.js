4884d5a010ce38e6fc5af2d035d218393608c717
amonradev <amonracardoso@gmail.com>
2021-11-23 20:22:44 -0300

    isso ta feito

---
+++ src/screens/Home/HomeScreens/History/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { View, Text } from 'react-native'
+
+export const History = () => {
+
+    return(
+        <View>
+            <Text>
+                History
+            </Text>
+        </View>
+    )
+}
