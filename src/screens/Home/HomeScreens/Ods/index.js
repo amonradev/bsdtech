d804eb8ea316366a37b425da22ab5b24bfee69ca
amonradev <amonracardoso@gmail.com>
2021-11-25 19:33:54 -0300

    try more

--- src/screens/Home/HomeScreens/Ods/index.js
+++ src/screens/Home/HomeScreens/Ods/index.js
@@ -1,26 +1,28 @@
 import axios from "axios"
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import { View, Text } from 'react-native'
 import { OdsList } from "../../../../components/HomeScreensComponents/OdsList"
 import { BackgroundImage } from "../../../../components/Main/BackgroundImg"
 import { Scroll } from "../../../../components/Scroll"
 
 export const Ods = () => {
-    const [dados, setDados] = useState([])
-    useEffect(async () => {
-            await axios({
+    const [data, setData] = useState([])
+    useEffect(() => {
+        async function fetchData() {
+            axios({
                 url: "https://node-amarrado.herokuapp.com/services",
                 method: "GET"
             }).then((res) => {
-                setDados(res.data)
+                setData(res.data)
             })
+        }
+        fetchData()
     }, [])
-    console.log(dados)
-    return(
+    return (
         <Scroll>
             <BackgroundImage>
                 <View>
-                    {dados.map((object) => {
+                    {data.map((object) => {
                         return <OdsList object={object} />
                     })}
                     <Text>
