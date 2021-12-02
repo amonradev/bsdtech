b8c8c08d93f5e69d16f8670ea1c6ebe9964d3c13
amonradev <amonracardoso@gmail.com>
2021-12-01 21:39:12 -0300

    Esperando api pro login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,69 +1,87 @@
-import React from "react"
-import { View, Text, ScrollView, TextInput, Image, StatusBar, TouchableOpacity, Button } from 'react-native'
+import React, { useContext, useEffect } from "react"
+import { View, Text, TextInput, Image, StatusBar, TouchableOpacity, Button, ScrollView } from 'react-native'
 import { style } from "./style"
 import { useForm, Controller } from "react-hook-form";
 import { UxButton } from "../../components/Crud/UxButton";
 import { Scroll } from "../../components/Scroll";
+import { ButtonSubmit } from "../../components/Crud/ButtonSubmit";
+import { SafeAreaView } from "react-native";
+import { UsuariosContext } from "../../context/UsuariosContext";
+import axios from "axios";
 
 
 export const Login = ({ navigation }) => {
 
-    const { control, handleSubmit, formState: { errors } } = useForm()
+    const { control, handleSubmit,  formState: { errors } } = useForm()
+    const { setFlag } = useContext(UsuariosContext)
 
-    const onSubmit = ({ data }) => {
-        console.log(data)
+    const onSubmit = (data) => {
+        axios({
+            url: "https://senaiuserapi.herokuapp.com/sessions",
+            method: "post",
+            data: data
+        }).then((res) => {   
+            if(res.data.flag) {
+                setFlag(res.data)
+                navigation.navigate('Main')
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
     }
+    
 
     return (
-        <Scroll>
-            <StatusBar backgroundColor="#1a202c" />
-            <View style={style.Box}>
-                <Image source={require('../../img/logoBsd.png')} style={style.Imagem} />
-                <Controller
-                    control={control}
-                    rules={{
-                        maxLength: 100,
-                        required: true
-                    }}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            placeholder="Digite seu email"
-                            style={style.input}
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                    name="email"
-                    defaultValue=""
-                />
-                {errors.email && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
+        <SafeAreaView>
+            <ScrollView>
+                <StatusBar backgroundColor="#1a202c" />
+                <View style={style.Box}>
+                    <Image source={require('../../img/logoBsd.png')} style={style.Imagem} />
+                    <Controller
+                        control={control}
+                        rules={{
+                            maxLength: 100,
+                            required: true
+                        }}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="Digite seu email"
+                                style={style.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                        name="email"
+                        defaultValue=""
+                    />
+                    {errors.email && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
 
-                <Controller
-                    control={control}
-                    rules={{
-                        maxLength: 100,
-                        required: true
-                    }}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            secureTextEntry={true}
-                            placeholder="Digite sua senha"
-                            style={style.input}
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                    name="senha"
-                    defaultValue=""
-                />
-                {errors.senha && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
-                <UxButton nome="Esqueceu sua senha?" estilo="marginTop: 0, textAlign: 'left' " />
-                <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-                <UxButton nome="Ainda não é cadastrado? Cadastre-se agora" />
-            </View>
-        </Scroll>
-
+                    <Controller
+                        control={control}
+                        rules={{
+                            maxLength: 100,
+                            required: true
+                        }}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                secureTextEntry={true}
+                                placeholder="Digite sua senha"
+                                style={style.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                        name="senha"
+                        defaultValue=""
+                    />
+                    {errors.senha && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
+                    <UxButton nome="Esqueceu sua senha?" func={() => navigation.navigate('Register')} estilo="marginTop: 0, textAlign: 'left' " />
+                    <ButtonSubmit nome="Fazer Login" onPress={handleSubmit(onSubmit)} />
+                    <UxButton nome="Ainda não é cadastrado? Cadastre-se agora" />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { View, Text, ScrollView, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, TextInput, TouchableOpacity, StatusBar, SafeAreaView } from 'react-native'
 import { style } from "./style"
 import { ButtonSubmit } from '../../components/Crud/ButtonSubmit'
 import { useForm, Controller } from "react-hook-form";
@@ -14,101 +14,95 @@ export const Register = ({ navigation }) => {
     console.log(data)
   }
   return (
-    <Scroll>
-      <StatusBar backgroundColor="#1a202c" />
-      <View style={style.Box}>
-        <Text style={style.Titulo}>Cadastre-se</Text>
-        <Controller
-          control={control}
-          rules={{
-            maxLength: 100,
-            required: true
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              placeholder="Digite seu nome"
-              style={style.input}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
-          )}
-          name="nome"
-          defaultValue=""
-        />
-        {errors.nome && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
+    <SafeAreaView>
+      <ScrollView>
+        <StatusBar backgroundColor="#1a202c" />
+        <View style={style.Box}>
+          <Text style={style.Titulo}>Cadastre-se</Text>
+          <Controller
+            control={control}
+            rules={{
+              maxLength: 100,
+              required: true
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Digite seu nome"
+                style={style.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="nome"
+            defaultValue=""
+          />
+          {errors.nome && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
 
-        <Controller
-          control={control}
-          rules={{
-            maxLength: 100,
-            required: true
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              placeholder="Digite seu email" style={style.input}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
-          )}
-          name="email"
-          defaultValue=""
-        />
-        {errors.email && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
-        <Controller
-          control={control}
-          rules={{
-            maxLength: 100,
-            required: true
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              secureTextEntry={true}
-              placeholder="Digite sua senha"
-              style={style.input}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
-          )}
-          name="senha"
-          defaultValue=""
-        />
-        {errors.senha && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
+          <Controller
+            control={control}
+            rules={{
+              maxLength: 100,
+              required: true
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Digite seu email" style={style.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="email"
+            defaultValue=""
+          />
+          {errors.email && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
+          <Controller
+            control={control}
+            rules={{
+              maxLength: 100,
+              required: true
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                secureTextEntry={true}
+                placeholder="Digite sua senha"
+                style={style.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="senha"
+            defaultValue=""
+          />
+          {errors.senha && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
 
-        <Controller
-          control={control}
-          rules={{
-            maxLength: 100,
-            required: true
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              secureTextEntry={true}
-              placeholder="Confirme sua senha"
-              style={style.input}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
-          )}
-          name="senhaConfirm"
-          defaultValue=""
-        />
-        {errors.senhaConfirm && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
+          <Controller
+            control={control}
+            rules={{
+              maxLength: 100,
+              required: true
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                secureTextEntry={true}
+                placeholder="Confirme sua senha"
+                style={style.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="senhaConfirm"
+            defaultValue=""
+          />
+          {errors.senhaConfirm && <Text style={style.TextError}>* Este campo é obrigatório</Text>}
 
-        <TouchableOpacity
-          style={style.Button}
-          onPress={handleSubmit(onSubmit)}
-        >
-          <Text style={style.TextButton}>
-            Criar minha conta
-          </Text>
-        </TouchableOpacity>
-        <UxButton nome="Já tem uma conta? Faça login" />
-      </View>
-    </Scroll>
-
+          <ButtonSubmit nome="Criar minha conta" onPress={handleSubmit(onSubmit)} />
+          <UxButton nome="Já tem uma conta? Faça login" func={() => navigation.navigate('Login')} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
